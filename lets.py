e8dc54711746732cc424483c896173e898093d05
ShriramSunder <ShriramSunder@da541cb1-d96a-4522-8c5c-30f57be1c076>
2015-04-05 14:16:44 +0000

    WORKS

--- lets.py
+++ lets.py
@@ -1,26 +1,26 @@
 import numpy as np
 import sys
 import math as m
-np.set_printoptions(precision=4)
+
 
 # EX= int(raw_input('Enter EX: '))
-EX = 20010000
+EX = 20010000.0
 # EY= int(raw_input('Enter EY: '))
-EY = 1301000
+EY = 1301000.0
 # GXY= int(raw_input('Enter GXY: '))
-GXY = 1001000
+GXY = 1001000.0
 # VXY= float(raw_input('Enter VXY: '))
 VXY = 0.3
 # VYX= float(raw_input('Enter VYX (enter 0 if only VXY): '))
 VYX = 0.02
 
-Q = np.array([[0,0,0],[0,0,0],[0,0,0]])
-Q_inv = np.array([[0,0,0],[0,0,0],[0,0,0]])
+Q = np.array([[0.0,0.0,0.0],[0.0,0.0,0.0],[0.0,0.0,0.0]])
+Q_inv = np.array([[0.0,0.0,0.0],[0.0,0.0,0.0],[0.0,0.0,0.0]])
 
 Q_MAJ = list()
-A = np.array([[0,0,0],[0,0,0],[0,0,0]])
-B = np.array([[0,0,0],[0,0,0],[0,0,0]])
-D = np.array([[0,0,0],[0,0,0],[0,0,0]])
+A = np.array([[0.0,0.0,0.0],[0.0,0.0,0.0],[0.0,0.0,0.0]])
+B = np.array([[0.0,0.0,0.0],[0.0,0.0,0.0],[0.0,0.0,0.0]])
+D = np.array([[0.0,0.0,0.0],[0.0,0.0,0.0],[0.0,0.0,0.0]])
 
 if(VYX!=0):
 	Q[0][0] = EX/(1-(VXY*VYX))
@@ -38,10 +38,10 @@ print("Q is now set up")
 print(Q)
 angles= [-m.pi/4,m.pi/4,m.pi/2,0,m.pi/2,0,0,m.pi/2,0,m.pi/2,m.pi/4,-m.pi/4]
 counter=0
-z=1
+Br = list()
 for al in angles:
 	counter=counter+1
-	print counter
+	# print counter
 	Q_inv[0][0] = Q[0][0]*pow(m.cos(al),4) + 2*(Q[0][1] + 2*Q[2][2])*pow(m.cos(al),2)*pow(m.sin(al),2) + Q[1][1]*pow(m.sin(al),4)
 
 	Q_inv[0][1] = (Q[0][0] + Q[1][1] - 4*Q[2][2])*pow(m.cos(al),2)*pow(m.sin(al),2) + Q[0][1]*(pow(m.cos(al),4) + pow(m.sin(al),4))
@@ -55,37 +55,35 @@ for al in angles:
 	Q_inv[2][2] = (Q[0][0] + Q[1][1] - 2*Q[0][1] - 2*Q[2][2])*pow(m.sin(al),2)*pow(m.cos(al),2)+ Q[2][2]*(pow(m.cos(al),4)+pow(m.sin(al),4))
 
 	Q_inv[1][0] = Q_inv[0][1]
-	# A[0][0] = A[0][0] +  Q_inv[i][j]*0.005
-
-	# Q_MAJ.append(Q_inv)
 
 
 	# print '\nQ_inv is now set up, and for al=',al,'it is:\n' 
 	# print(Q_inv)
-	for i,j in zip([0,0,1,0,1,2],[0,1,1,2,2,2]):
+
+	for i,j in zip([1,0,0,1,0,1,2],[0,0,1,1,2,2,2]):
 		A[i][j]= A[i][j]+  Q_inv[i][j]*0.005
+		D[i][j]= D[i][j] + Q_inv[i][j]*(pow(0.005,3)/12 + (0.005)*pow(0.0025,2))
 		if(counter<7):
-			B[i][j]= B[i][j] + Q_inv[i][j]*0.005*(z*0.0025)
+			B[i][j]= B[i][j] + Q_inv[i][j]
 		else:
-			z=-1
-			B[i][j]= B[i][j] + Q_inv[i][j]*0.005*(z*0.0025)
-		
+			B[i][j]= B[i][j] - Q_inv[i][j]
+		# print 'B:', B ,'\n'
+	# Br.append(B[0][0])
 
 	# 	print '\nA[',i,'][',j,']','=','A[',i,'][',j,'] +', 'Q_inv[',i,'][',j,']'
 	# print '\nA for ply',al,'is set up\n'
 	# print A ,'\n'
 			
-	# print 'A:\n'
-	# print(A)
 	# raw_input()
-	
-	# Q_inv # End loop
-# 	# B[i][j]= B[i][j] + Q_inv[i][j]*0.005*()
-print '\nQ_inv is now set up, and for al=',al,'it is:\n' 
-print(Q_inv)	
+# print 'Br: \n'
+# print Br
+np.set_printoptions(precision=4, suppress=True)
 print 'A:\n'
 print(A)
+
 print 'B:\n'
 print(B)
-# for i in range(1,3):
-# 	for j in range(1,3):
+
+np.set_printoptions(precision=4, suppress=False)
+print 'D:\n'
+print(D)
