7323804f09367565995b70bc382559929dfe3ab4
Shriram Sunder <shriram.sunder121091@gmail.com>
2015-07-25 15:41:09 +0100

    Before First(full batch) run, and before validation

---
+++ Mo/Asymmetric/input_32cell/fullopt/remove_unwanted.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+rm -r input.optstory/
+rm input.xyz
+rm input.f*
+rm input.SCFLOG
+rm input.gui
+rm input.OPTINFO
+
+rm fort* 
+rm first* 
+
+rm e* 
+rm machines.LINUX
+rm SCFOUT.LOG
---
+++ Nb/Symmetric/input_16cell/fullopt/remove_unwanted.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+rm -r input.optstory/
+rm input.xyz
+rm input.f*
+rm input.SCFLOG
+rm input.gui
+rm input.OPTINFO
+rm input.out
+
+rm fort* 
+rm first* 
+
+rm e* 
+rm machines.LINUX
+rm SCFOUT.LOG
